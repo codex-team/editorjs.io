7b8b6fbaa5b2f54db1c4a9cc04db83b8a2d913fe
slaveeks <chernyshev2001@icloud.com>
2022-10-18 12:06:12 +0300

    Added link for button

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -1,11 +1,13 @@
 <template>
-  <button class="button" :class="{
+  <a :href="link">
+    <button class="button" :class="{
     'button--support': type === 'support',
     'button--primary': type === 'primary',
   }">
-    <img v-if="icon" :src="icon" class="button__icon"/>
-    {{ text }}
-  </button>
+      <img v-if="icon" :src="icon" class="button__icon"/>
+      {{ text }}
+    </button>
+  </a>
 </template>
 
 <script setup lang="ts">
@@ -24,32 +26,37 @@ defineProps<{
    * Button text
    */
   text: String,
+
+  /**
+   * Button link
+   */
+  link?: String,
 }>()
 </script>
 
 <style scoped>
-  .button {
-    display: flex;
-    align-items: center;
-    border: 0;
-    color: white;
-    cursor: pointer;
-  }
+.button {
+  display: flex;
+  align-items: center;
+  border: 0;
+  color: white;
+  cursor: pointer;
+}
 
-  .button--support {
-    background: linear-gradient(270deg, #E858FF 0%, #9B42F3 82.81%);
-    border-radius: 8px;
-    padding: 6px 9px;
+.button--support {
+  background: linear-gradient(270deg, #E858FF 0%, #9B42F3 82.81%);
+  border-radius: 8px;
+  padding: 6px 9px;
 
-    .button__icon {
-      margin-right: 5px;
-    }
+  .button__icon {
+    margin-right: 5px;
   }
+}
 
-  .button--primary {
-    background-color: var(--color-button-primary);
-    box-shadow: 0 8px 25px rgba(28, 173, 255, 0.46);
-    border-radius: 10px;
-    padding: 14px 26px;
-  }
+.button--primary {
+  background-color: var(--color-button-primary);
+  box-shadow: 0 8px 25px rgba(28, 173, 255, 0.46);
+  border-radius: 10px;
+  padding: 14px 26px;
+}
 </style>
