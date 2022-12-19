f3ace02d2863391047adf16a25a466bb33bf64d3
slaveeks <chernyshev2001@icloud.com>
2022-12-19 17:18:42 +0300

    Added enum for analitycs events

---
+++ utils/analytics/index.ts
@@ -0,0 +1,11 @@
+enum AnalyticEvent {
+  GetStartedClicked = 'Get Started — Clicked',
+  DocumentationClicked = 'Documentation - Clicked',
+  AwesomePluginsClicked = 'Awesome plugins - Clicked',
+  SupportEditorJSClicked = 'Support Editor.js - Clicked',
+  SponsorClicked = 'Sponsor - Clicked',
+  StarClicked = 'Star - Clicked',
+  JoinClicked = 'Join us - Clicked'
+}
+
+export default AnalyticEvent;
